ccf067ecb0b66028ff845d836f55395f71f3af69
ImaNestoDostupno <ujovanovic@hotmail.com>
2023-11-11 13:26:47 +0100

    Ovo je program proracuna zapremine valjka

---
+++ src/ZapreminValjka.java
@@ -0,0 +1,10 @@
+public class ZapreminValjka {
+    public static void main (String [] args) {
+        int R = 10; // Poluprecnik Valjka
+        int H = 50; // Visina Valjka
+        final double PI = 3.14; // Konstanta PI
+        double V = (R*R) * PI * H ; // Proracun zapremine Valjka
+        System.out.println(V); // Uradjena implicitna ( automacka ) konverzija
+
+    }
+}
